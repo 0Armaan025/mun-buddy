911f122cfe55b84cf5af404b6e86b2eac805bc76
unknown <0Armaan025>
2024-04-22 22:23:11 +0530

    im stupid

--- src/components/research_tile/ResearchTile.jsx
+++ src/components/research_tile/ResearchTile.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const ResearchTile = () => {
-  return <></>;
+  return (
+    <>
+      <div
+        className="researchTile flex flex-col justify-center hover:pointer-cursor cursor-pointer hover:bg-blue-950 transition-all pointer-cursor items-center p-6 bg-blue-900 rounded text-white"
+        style={{ fontFamily: "Poppins" }}
+      >
+        <h2 className="hover:pointer-cursor">+</h2>
+        <h2 className="hoverpointer-cursor"> Research </h2>
+      </div>
+    </>
+  );
 };
 
 export default ResearchTile;
--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import DashboardSideBar from "../../components/sidebar/dashboard.sidebar";
+import ResearchTile from "../../components/research_tile/ResearchTile";
 
 const DashboardPage = () => {
   return (
@@ -8,11 +9,15 @@ const DashboardPage = () => {
       <Navbar />
       <div className="dashboard flex flex-row justify-start items-start">
         <DashboardSideBar />
-        <div className="dashboardDiv ml-4">
+        <div className="dashboardDiv ml-4 flex flex-col justify-start items-start">
           <h2 style={{ fontFamily: "Poppins" }} className="text-white text-2xl">
             {" "}
             Good evening, Armaan! 👋{" "}
           </h2>
+        </div>
+        <br />
+        <div className="researchRow flex mt-16 flex-row absolute left-44">
+          <ResearchTile />
         </div>
       </div>
     </>
